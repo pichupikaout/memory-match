ae8e286bf406521d59c942a94e328500e7a01495
pichupikaout <30759044+pichupikaout@users.noreply.github.com>
2020-11-07 16:42:02 -0500

    Update Memory_Match.java

--- Memory_Match.java
+++ Memory_Match.java
@@ -1,11 +1,17 @@
 import java.util.*; 
-public class Memory_Match
-{    
+public class Memory_Match {
+public static final int NUM_PAIRS = 4;
 public static void main() {
-        final int NUM_PAIRS = 4;
+        boardCreate();
+    }
+public static void boardCreate() { // creation and shuffling of the board
+        
         int board[] = new int[NUM_PAIRS*2+1];
         boolean printElement[] = new boolean[NUM_PAIRS*2+1];
-        int temp, position1, position2,i;
+        int temp = 0;
+        int position1 = 0;
+        int position2 = 0;
+        int i = 0;
         int k=1;
         for (i=1; i<=NUM_PAIRS*2; i+=2) {
             board[i] = k;
@@ -20,93 +26,99 @@ public static void main() {
               board[position1] = board[position2];
               board[position2] = temp;
         }
-        //the actual game begins
+        prompt(board, printElement);
+    }
+public static void prompt(int[] board, boolean[] printElement) { //the actual game begins here
         Scanner console = new Scanner(System.in);
-        int count = 0;
+        int numbersFlipped = 0;
         String playerOne = "";
         String playerTwo = "";
         int playerOnescore = 0;
         int playerTwoscore = 0;
-        int guess1 = 0;
-        int guess2 = 0;
-        int decider = 1;
-        boolean flag = false;
+        boolean player = true;
         System.out.println("Player 1's name? ");
         playerOne = console.next();
         System.out.println("Player 2's name? ");
         playerTwo = console.next();
-        for (i=0; i<board.length; i++) { // prints out board with nothing
+        for (int i=0; i<board.length; i++) { // prints out board with nothing
             if (printElement[i]==true)
                 System.out.print(board[i] +  "  ");
             else
                 System.out.print(" * ");
         }
         System.out.println();
-        System.out.println(Arrays.toString(board));
-        while (count < board.length) { // checks if the entire board is flipped or not
-            for (int p = 1; p < board.length; p++) {
-                if (printElement[p] == true) {
-                    count++;
-                }
-            }
-            if (decider % 2 == 1) { // player 1's turn
-                 System.out.println(playerOne + ", what is your 1st choice? ");
-                 System.out.println();
-                 guess1 = console.nextInt();
-                 decider = game(guess1, guess2, decider, playerOnescore, playerTwoscore, playerOne, playerTwo, printElement, board);
-                 if (decider % 2 == 1) {
+        while (numbersFlipped <= board.length - 1) { // checks if the entire board is flipped or not
+            for (int p = 1; p < board.length; p++) { // the actual algorithm 
+            if (player == true) { // player 1's turn
+                player = game(playerOnescore, playerTwoscore, playerOne, playerTwo, printElement, board, player);
+                if (player == true) { //checks if player 1 won
                     playerOnescore++;
+                    numbersFlipped+=2;
                 }
                 System.out.println(playerOne + ": " + playerOnescore + " points");
                 System.out.println(playerTwo + ": " + playerTwoscore + " points");
             } else { // player 2's turn
-                System.out.println(playerTwo + ", what is your 1st choice? ");
-                System.out.println();
-                guess1 = console.nextInt();
-                decider = game(guess1, guess2, decider, playerOnescore, playerTwoscore, playerOne, playerTwo, printElement, board);
-                if (decider % 2 == 0) {
+                player = game(playerOnescore, playerTwoscore, playerOne, playerTwo, printElement, board, player);
+                if (player == false) { //checks if player 2 won
                     playerTwoscore++;
+                    numbersFlipped+=2;
                 }
                 System.out.println(playerOne + ": " + playerOnescore + " points");
                 System.out.println(playerTwo + ": " + playerTwoscore + " points");
             }
             System.out.println();
         }
-}      
-public static int game(int guess1, int guess2, int decider, int playerOnescore, int playerTwoscore, String playerOne, String playerTwo, boolean[] printElement, int[] board) {
+    } 
+}     
+public static boolean game(int playerOnescore, int playerTwoscore, String playerOne, String playerTwo, boolean[] printElement, int[] board, boolean player) {
     Scanner console = new Scanner(System.in);
-    int playerOnes = 0;
-    int playerTwos = 0;
-    while (printElement[guess1] == true) {
-    System.out.println("You sly fox, try again");
-    System.out.println("What is your 1st choice? ");
+    int guess1 = 0;
+    int guess2 = 0;
+    if (player == true) {
+        System.out.println(playerOne + ", what is your 1st choice? ");
+        System.out.println();
+    } else {
+        System.out.println(playerTwo + ", what is your 1st choice? ");
+        System.out.println();
+        
+    }
     guess1 = console.nextInt();
+    while (printElement[guess1] == true) { //checks if a sneaky man decides to choose an already flipped number on the 1st guess
+        System.out.println("You sly fox, try again");
+        if (player == true) {
+            System.out.println(playerOne + ", what is your 1st choice? ");
+        } else {
+            System.out.println(playerTwo + ", what is your 1st choice? ");
+        }
+        guess1 = console.nextInt(); 
     }
     printElement[guess1] = true;
-    for (int a = 1; a < board.length; a++) {
+    for (int a = 1; a < board.length; a++) { 
         if (printElement[a] == true) {
             System.out.print(board[a] + " ");
         } else {
             System.out.print(" * ");
         }     
     }
-    System.out.println("What is your 2nd choice? ");
-    System.out.println();
+    if (player == true) {
+            System.out.println(playerOne + ", what is your 2nd choice? ");
+            System.out.println();
+        } else {
+            System.out.println(playerTwo + ", what is your 2nd choice? ");
+            System.out.println();
+        }
     guess2 = console.nextInt();
-    while (printElement[guess2] == true) { //checks if a sneaky man chooses an already flipped number on his second choice
+    while (printElement[guess2] == true) { //checks if a sneaky man decides to choose an already flipped number on the 2nd guess
         System.out.println("You sly fox, try again");
-        System.out.println("What is your 2nd choice? ");
-        guess2 = console.nextInt();
+        if (player == true) {
+            System.out.println(playerOne + ", what is your 2nd choice? ");
+        } else {
+            System.out.println(playerTwo + ", what is your 2nd choice? ");
+        }
+        guess2 = console.nextInt(); 
     }
     if (board[guess1] == board[guess2]) { // if both choices match
         printElement[guess2] = true;
-        if (decider % 2 == 1) {
-            playerOnes++; 
-            
-        } else {
-            playerTwos++;
-            
-        }
         for (int a = 1; a < board.length; a++) {
             if (printElement[a] == true) {
                 System.out.print(board[a] + " ");
@@ -114,7 +126,13 @@ public static int game(int guess1, int guess2, int decider, int playerOnescore, 
                 System.out.print(" * ");
             }
         }
-        System.out.println("MATCH!!! YOU GOT A MATCH!!! " + "go again!!!");
+        if (player == true) {
+            System.out.println("MATCH!!!!! " + playerOne + " goes again!");
+            System.out.println();
+        } else {
+            System.out.println("MATCH!!!!! " + playerTwo + " goes again!");
+            System.out.println();
+        }
     } else { // if it doesn't, goes to next player
         printElement[guess1] = false;
         for (int a = 1; a < board.length; a++) {
@@ -125,8 +143,12 @@ public static int game(int guess1, int guess2, int decider, int playerOnescore, 
             }
         }
         System.out.println("No match");
-        decider++;
+        if (player == true) {
+            player = false;
+        } else {
+            player = true;
+        }
     }
-    return decider;
+    return player;
     }
 }
